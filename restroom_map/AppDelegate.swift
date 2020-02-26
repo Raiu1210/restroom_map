20c0c265e5cc3571c5129f507055750fa8a0fcd7
Raiu1210 <baio1484@gmail.com>
2020-02-26 17:28:58 +0900

    GET restroom_location from server

--- restroom_map/AppDelegate.swift
+++ restroom_map/AppDelegate.swift
@@ -16,7 +16,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        api.sayHello()
+        api.get_restroom_list()
         return true
     }
 
--- restroom_map/script/API.swift
+++ restroom_map/script/API.swift
@@ -10,7 +10,51 @@ import Foundation
 
 
 class API {
+    let server_url = "http://zihankimap.work/"
+    
+    let encoder = JSONEncoder()
+    let decoder = JSONDecoder()
+    
     func sayHello() {
         print("hello")
     }
+    
+    func get_restroom_list() {
+        let semaphore = DispatchSemaphore(value: 0)
+        let destination = server_url + "datalist?table_id=2"
+        let url = URL(string: destination)!
+        
+        let session = URLSession.shared
+        let request = URLRequest(url: url)
+        
+        
+        session.dataTask(with: request) {
+            (data, response, error) in if error == nil, let data = data, let _ = response as? HTTPURLResponse {
+                
+                do {
+//                    let jsonString = String(data: data, encoding: String.Encoding.utf8) ?? ""
+//                    print(jsonString)
+                    print(data)
+                    let recv_obj: [Restroom_data] = try! self.decoder.decode([Restroom_data].self, from: data)
+                    print(recv_obj)
+
+                    semaphore.signal()
+                } catch {
+                    print("Error:\(error)")
+                }
+                    
+                }
+            }.resume()
+        semaphore.wait()
+    }
+}
+
+
+
+struct Restroom_data : Codable {
+    var id:String
+    var latitude:String
+    var longitude:String
+    var updated:String
+    var memo:String
 }
